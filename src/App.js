7fd1f00bb20e20dacc6854f8892d951debc131e6
sagar411 <sagar.sunar411@gmail.com>
2022-03-16 21:57:44 +0545

    first routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 
 import './App.scss';
 import HomePage from './pages/homepage/homepage.component';
+import {Routes,Route} from 'react-router-dom'
 
+const HatsPage =()=>(
+  <div>
+    <h1>hats page</h1>
+  </div>
+);
 function App() {
   return (
     <div >
-    <HomePage></HomePage>
+    
+      <Routes>
+      <Route  exact path='/' element={<HomePage/>}></Route>
+      <Route path='/hats' element={<HatsPage/>}></Route>
+      </Routes>
+      
+    {/* <HomePage></HomePage> */}
     </div>
   );
 }
