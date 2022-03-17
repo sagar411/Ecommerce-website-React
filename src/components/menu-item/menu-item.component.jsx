615015a854a8fbeffe320ecc7b04ea2baea8c0c6
sagar411 <sagar.sunar411@gmail.com>
2022-03-17 21:14:53 +0545

    react routing-useNavigate

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,17 +1,25 @@
 import React from "react";
 import './menu-item.styles.scss'
+import { useNavigate } from "react-router-dom";
 
+const MenuItem=({title,imageUrl,size,linkUrl,match})=>{
+    const navigate = useNavigate();
 
-const MenuItem=({title,imageUrl,size})=>{
+    const onClick=()=>{
+        setTimeout(()=>{
+            navigate(`${match.url}${linkUrl}`);
+        })
+    }
+
  return(
-    <div className={`${size} menu-item`}>
+    <div className={`${size} menu-item`} onClick={onClick}>
         <div className="background-image"  style={{
         backgroundImage: `url(${imageUrl})`
     }}></div>
     <div className="content">
         <h1 className="title">{title.toUpperCase()}</h1>
         <span className="subtitle">SHOP NOW</span>
-    </div>
+    </div> 
 </div>
  )
 }
